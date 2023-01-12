8f71b100f39be40f8f3d12aec31bbb21c5e1d48c
Kevin Seifert <kevin.seifert.berlin@googlemail.com>
2023-01-12 17:56:40 +0100

    adds supermarket.py skeleton to project

---
+++ supermarket.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+class Supermarket:
+
+    def __init__(self) -> None:
+        pass
+
+    def move_customer(self):
+        pass
+
+    def check_state(self):
+        pass
+
+    def checkout_customer(self):
+        pass
+
+    def add_customer(self):
+        pass
+
+    def open_market(self):
+        pass
+
+    def close_market():
+        pass
+
+    def calculate_revenue(self):
+        pass
+
+    def pick_products(self):
+        pass
