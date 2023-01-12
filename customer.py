2e36600e122665696d7b723a7d351422aa7b28aa
Kevin Seifert <kevin.seifert.berlin@googlemail.com>
2023-01-12 22:17:27 +0100

    corrects quotes from docstrings from

--- customer.py
+++ customer.py
@@ -5,9 +5,9 @@ from products import Products
 class Customer:
 
     def __init__(self, tm, entry, timestamp) -> None:
-        """"
+        """
         Initiate the Customer
-        """"
+        """
         self.tm = tm
         self.destination = self.tm.columns
         self.location = self.next_location(self.destination, entry)
@@ -22,9 +22,9 @@ class Customer:
         new_location = np.random.choice(self.destination, p=probs)
 
     def move(self, timestamp):
-        """"
+        """
         Moves the customer to a random location
-        """"
+        """
         probs = tm[tm.index == self.location].values[0] # da es ein doppelarray ist, möchte ich den 1. bekommen
         new_location = self.next_location(destination, probs)
 
@@ -34,17 +34,17 @@ class Customer:
 
 
     def pick_product(self):
-        """"
+        """
         Picks a random product
-        """"
+        """
         department_products = self.prd.shopping_department[self.location]
         product_choice = np.random.choice(list(department_products.keys()))
         self.buy_or_steal()
 
     def buy_or_steal(self, product, price):
-        """"
+        """
         Buy or steal product and add them to the corresponding list
-        """"
+        """
         if np.random.uniform() > 0.8:
             print(f"The customer stole {product}.")
             self.stolen_article = self.stolen_article.append({product:price})
