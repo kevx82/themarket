dc66084362e4828abc8b04b8990c4e5ba5fc15c2
Kevin Seifert <kevin.seifert.berlin@googlemail.com>
2023-01-13 02:11:13 +0100

    changes transition to dict with extra key id, adds comments to move, comments out related code to Products module

--- customer.py
+++ customer.py
@@ -1,38 +1,53 @@
 import pandas as pd
 import numpy as np
-from products import Products
+#from products import Products
 
 class Customer:
 
-    def __init__(self, tm, entry, timestamp) -> None:
+    def __init__(self, tm, entry, customer_no) -> None:
         """
         Initiate the Customer
         """
         self.tm = tm
-        self.destination = self.tm.columns
-        self.location = self.next_location(self.destination, entry)
+        self.entry = entry
+        self.customer_no = customer_no
+
+        self.destination = self.tm.columns.to_list()
+        self.location = self.next_location(self.entry)
 
         self.shopping_cart = []
         self.stolen_article = []
-        self.transition = []
-        self.move(timestamp)
-        self.prd = Products()
+        # dict for all transitions
+        self.transition = {'timestamp': [], 'location': [], 'customer_no': []}
+
+        #self.prd = Products()
 
     def next_location(self, probs):
-        new_location = np.random.choice(self.destination, p=probs)
+         return np.random.choice(self.destination, p=probs)
+
+    def add_transition(self, timestamp):
+        self.transition['timestamp'].append(timestamp)
+        self.transition['location'].append(self.location)
+        self.transition['customer_no'].append(self.customer_no)
 
     def move(self, timestamp):
         """
         Moves the customer to a random location
         """
-        probs = tm[tm.index == self.location].values[0] # da es ein doppelarray ist, möchte ich den 1. bekommen
-        new_location = self.next_location(destination, probs)
+        # getting probabilities for destinations of current location
+        # da es ein doppelarray ist, möchte ich den 1. bekommen
+        probs = self.tm[self.tm.index == self.location].values[0]
+        # setting possible new location
+        new_location = self.next_location(probs)
 
+         # checking for new location
         if new_location != self.location:
-            self.transition.append({timestamp:new_location})
+            # adding new position to transition list
+            self.add_transition(timestamp)
+        
+        # changing current position to new location
         self.location = new_location
-
-
+'''
     def pick_product(self):
         """
         Picks a random product
@@ -51,3 +66,4 @@ class Customer:
         else: 
             print(f"The customer add {product} to the shopping cart.")
             self.shopping_cart = self.shopping_cart.append({product:price})
+'''
