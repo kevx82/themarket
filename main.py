1be2d3c2229844acb3520a14273abeb16e517e7b
Kevin Seifert <kevin.seifert.berlin@googlemail.com>
2023-01-13 02:09:26 +0100

    changes day, adds close_market and prints some outputs

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from supermarket import Supermarket
 logging.basicConfig(level=logging.INFO)
 
 path = 'data/'
-day = 'monday'
+day = 'friday'
 
 
 if __name__ == '__main__':
@@ -21,10 +21,14 @@ if __name__ == '__main__':
     tmgen = TMGenerator(path=path)
 
     # initiate the Supermarket
-    super_market = Supermarket(tm=tmgen.tm_dict[day], date="2022-01-02")
+    super_market = Supermarket(tm=tmgen.tm_dict[day], entry=tmgen.entry_dict[day], date="2022-01-02")
 
     # print transition matrix for monday
     logging.info(tmgen.tm_dict[day])
     logging.info(tmgen.entry_dict[day])
     super_market.open_market()
+    super_market.close_market()
+    day_df = pd.concat([pd.DataFrame(cust.transition) for cust in super_market.customers_inactive], ignore_index=True)
+    print(day_df[day_df['customer_no'] == 1])
+    print([cust.transition for cust in super_market.customers_inactive[:5]])
 
